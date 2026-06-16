72d9ec80585112642760a2237b25316f5273b282
Durai Karthik S <karthickkanna2006@gmail.com>
2026-06-16 11:45:49 +0530

    Add files via upload

--- match.py
+++ match.py
@@ -2,11 +2,17 @@ s1 = input()
 s2=input()
 i=0
 j=0
+l1=[]
+l2=[]
 while i<len(s1) and j<len(s2):
         if s1[i] == s2[j]:
             i+=1
             j+=1
         elif s1[i] != s2[j]:
-            print(s1[i])
+            l1.append(s1[i])
+            l2.append(s2[j])
             i+=1
             j+=1
+
+print(l1)
+print(l2)
