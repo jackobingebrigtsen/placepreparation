6f8ab13df79bb9bc990dda242c8ccdc4265c62c4
Durai Karthik S <karthickkanna2006@gmail.com>
2026-06-17 20:49:51 +0530

    Add files via upload

--- pattern.py
+++ pattern.py
@@ -1,7 +1,9 @@
-rows=5
-for i in range(1,rows+1):
-    for j in range(rows-i):
-        print("",end="")
-    for k in range(i):
-        print(f"{i}",end="")
-    print()
+n=5
+for i in range(0,n+1):
+    for j in range(n-i+1,n+1,1):
+        print(j,end="")
+    print(0,end="")
+    for j in range(n,n-i,-1):
+        print(j,end="")
+    print()    
+    
